ad60572ff054a27d9a48b3abe91d7162e1507ef7
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-13 05:45:57 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkT8uAVMgFfKMEQLl82ZYcmMrQJwra92Q",
+  authDomain: "naxchat-27309.firebaseapp.com",
+  projectId: "naxchat-27309",
+  storageBucket: "naxchat-27309.firebasestorage.app",
+  messagingSenderId: "138730719673",
+  appId: "1:138730719673:web:7761e0c196d563f5164e68"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
