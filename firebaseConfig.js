88fc25862aa97944b70b6aa376e1d3872eca8f47
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-13 06:37:05 +0530

    Change Firebase apiKey in firebaseConfig.js
    Updated the apiKey in firebaseConfig.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkT8uAVMgFfKMEQLl82ZYcmMrQJwra92Q",
+  apiKey: "AIzaSyCkT8uAVMgFfkMEQLI82ZYcmMrQJwra92Q",
   authDomain: "naxchat-27309.firebaseapp.com",
   projectId: "naxchat-27309",
   storageBucket: "naxchat-27309.firebasestorage.app",
@@ -11,9 +11,6 @@ const firebaseConfig = {
   appId: "1:138730719673:web:7761e0c196d563f5164e68"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Auth & Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
