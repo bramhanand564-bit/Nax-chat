c550d17bb3512d97a22b7572b6dd4e1a5890098d
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-13 20:50:04 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForNaxChatBuild-12345",
-  authDomain: "nax-chat.firebaseapp.com",
-  projectId: "nax-chat",
-  storageBucket: "nax-chat.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef"
+  apiKey: "AIzaSyCkT8uAVMgFfkMEQLI82ZYcmMrQJwra92Q",
+  authDomain: "naxchat-27309.firebaseapp.com",
+  projectId: "naxchat-27309",
+  storageBucket: "naxchat-27309.firebasestorage.app",
+  messagingSenderId: "138730719673",
+  appId: "1:138730719673:web:7761e0c196d563f5164e68"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
